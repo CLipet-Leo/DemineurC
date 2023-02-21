ced219cc416446af0bb18e88f096b89f1aec275f
Leo <lclipet@gaming.tech>
2023-02-21 18:54:00 +0100

    mine de tes mort

--- test.c
+++ test.c
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 char JEU[10][10];//grille de jeu 
-int i, j;//i les lignes, j les colonnes
+int i, j, b;//i les lignes, j les colonnes
+
+void random_bomb()
+{
+    srand(time(NULL));
+    for (b = 0; b < 10; b++) {
+        int previ = rand() % 10;
+        int prevj = rand() % 10;
+        JEU[previ][prevj] = 'x';
+        //printf("x=%d et y=%d \n", previ, prevj);
+    }
+}
 
 int main()
 {
@@ -15,8 +29,12 @@ int main()
             printf("%d | ", i + 1);
         for (j = 0; j < 10; j++)
         {
+            JEU[i][j] = '*';
+            random_bomb();
             printf("%c  ", JEU[i][j]);
         }
         printf("\n");
     }
+
+    return 0;
 }
