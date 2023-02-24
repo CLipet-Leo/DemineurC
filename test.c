d48ea5923ccd4ac847ac30ef90d4a4de615d9193
Leo <lclipet@gaming.tech>
2023-02-24 10:20:18 +0100

    Debug premier placement + calcul mine fonctionnel

--- test.c
+++ test.c
@@ -4,9 +4,11 @@
 
 char JEU[10][10];//grille de jeu 
 int x, y;//x les lignes, y les colonnes
-char MINE[10][10];//la grille avec les mines
+int MINE[10][10];//la grille avec les mines
 int nbMine;//le nombre de mine
 int premier = 0;
+int ligne;
+int colonne;
 
 void init()//fonction d'initialisation des grilles
 {
@@ -15,7 +17,7 @@ void init()//fonction d'initialisation des grilles
         for (y = 0; y < 10; y++)
         {
             JEU[x][y] = '-';//affiche la grille de jeu avec des '-'
-            MINE[x][y] = '0';//définis la grille de mine avec des '0'
+            MINE[x][y] = 0;//définis la grille de mine avec des 0
         }
     }
 }
@@ -24,13 +26,13 @@ void random_mine()//fonction de placement aléatoire d'un nombre de mine définis
 {
     srand(time(NULL));
     int m;
-    nbMine = 20;//nombre de mine à placé sur la grille MINE
+    nbMine = 10;//nombre de mine à placé sur la grille MINE
     for (m = 0; m < nbMine; m++) {
         x = rand() % 10;
         y = rand() % 10;
-        if (MINE[x][y] == '0')//On vérifie que la case est libre
+        if (MINE[x][y] == 0 || MINE[x][y] != MINE[ligne][colonne])//On vérifie que la case est libre
         {
-            MINE[x][y] = 'X';//on place un X pour indiquer qu'il y a une mine
+            MINE[x][y] = 1;//on place un 1 pour indiquer qu'il y a une mine
         }
         else {
             nbMine++;
@@ -66,19 +68,13 @@ void afficherMine() {
             printf("%d | ", x + 1);
         for (y = 0; y < 10; y++)
         {
-            printf("%c  ", MINE[x][y]);
+            printf("%d  ", MINE[x][y]);
         }
         printf("\n");
     }
 }
 
-int checkMine() {
-    return MINE[x - 1][y - 1] + MINE[x - 1][y] + MINE[x - 1][y + 1] + MINE[x][y - 1] + MINE[x][y + 1] + MINE[x + 1][y - 1] + MINE[x + 1][y] + MINE[x + 1][y + 1];
-}
-
 void play() {
-    int ligne;
-    int colonne;
     while (premier == 0) {
         printf("\nEntrez votre premiere case a jouer");
         printf("\nla ligne : "); scanf_s("%d", &ligne);
@@ -87,7 +83,7 @@ void play() {
         ligne -= 1;
         colonne -= 1;
         if (JEU[x][y] <= JEU[ligne][colonne]) {
-            JEU[ligne][colonne] = '0';
+            JEU[ligne][colonne] = '*';
             premier++;
         }
         else {
@@ -95,6 +91,10 @@ void play() {
             break;
         }
     }
+}
+
+int checkMine(int x, int y) {
+    return MINE[x - 1][y - 1] + MINE[x - 1][y] + MINE[x - 1][y + 1] + MINE[x][y - 1] + MINE[x][y + 1] + MINE[x + 1][y - 1] + MINE[x + 1][y] + MINE[x + 1][y + 1];
 }
 
 int main()
@@ -108,9 +108,9 @@ int main()
     init();//intialisation des grilles de jeu
     afficherJeu();
     play();
-    afficherJeu();
     random_mine();
-    checkMine();
+    afficherJeu();
+    printf("\n\nIl y a %d mines autour de ta case", checkMine(ligne, colonne));
     afficherMine();
 
 
