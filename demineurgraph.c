288289e9fe80969227c41ef6bc19f5fc888764eb
Leo <lclipet@gaming.tech>
2023-03-07 16:29:31 +0100

    Affichage d'une case

--- demineurgraph.c
+++ demineurgraph.c
@@ -1,35 +1,105 @@
 #include <SDL.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>  // <cstdbool> en C++
 
-int SDL_Init(Uint32 flags);
-void SDL_Quit(void);
-const char* SDL_GetError(void);
-void SDL_Delay(Uint32 ms);
+
+//The window we'll be rendering to
+SDL_Window* window = NULL;
+//The surface contained by the window
+SDL_Surface* screenSurface = NULL;
+//The image we will load and show on the screen
+SDL_Surface* tile = NULL;
+
+
+bool init()
+{
+    //Initialization flag
+    bool success = true;
+
+    //Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        success = false;
+    }
+    else
+    {
+        //Create window
+        window = SDL_CreateWindow("SDL Tests", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
+        if (window == NULL)
+        {
+            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+            success = false;
+        }
+        else
+        {
+            //Get window surface
+            screenSurface = SDL_GetWindowSurface(window);
+        }
+    }
+
+    return success;
+}
+
+bool loadMedia()
+{
+    //Loading success flag
+    bool success = true;
+
+    //Load splash image
+    tile = SDL_LoadBMP("../../img/tile.bmp");
+    if (tile == NULL)
+    {
+        printf("Unable to load image %s! SDL Error: %s\n", "../../img/tile.bmp", SDL_GetError());
+        success = false;
+    }
+
+    return success;
+}
+
+void close()
+{
+    //Deallocate surface
+    SDL_FreeSurface(tile);
+    tile = NULL;
+
+    //Destroy window
+    SDL_DestroyWindow(tile);
+    tile = NULL;
+
+    //Quit SDL subsystems
+    SDL_Quit();
+}
 
 
 
 int main(int argc, char* argv[]) {
-    SDL_Window* window = NULL;
-    int statut = EXIT_FAILURE;
-
-    if (0 != SDL_Init(SDL_INIT_VIDEO))
+    //Start up SDL and create window
+    if (!init())
     {
-        fprintf(stderr, "Erreur SDL_Init : %s", SDL_GetError());
-        goto Quit;
+        printf("Failed to initialize!\n");
     }
-    window = SDL_CreateWindow("Deminator", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-        640, 480, SDL_WINDOW_SHOWN);
-    if (NULL == window)
+    else
     {
-        fprintf(stderr, "Erreur SDL_CreateWindow : %s", SDL_GetError());
-        goto Quit;
+        //Load media
+        if (!loadMedia())
+        {
+            printf("Failed to load media!\n");
+        }
+        else
+        {
+            //Apply the image
+            SDL_BlitSurface(tile, NULL, screenSurface, NULL);
+            //Update the surface
+            SDL_UpdateWindowSurface(window);
+            //Hack to get window to stay up
+            SDL_Event e; bool quit = false; while (quit == false) { while (SDL_PollEvent(&e)) { if (e.type == SDL_QUIT) quit = true; } }
+        }
     }
 
-    statut = EXIT_SUCCESS;
-    SDL_Delay(3000);
-    SDL_DestroyWindow(window);
-Quit:
-    SDL_Quit();
-    return statut;
+    //Free resources and close SDL
+    close();
+
+    return 0;
 }
